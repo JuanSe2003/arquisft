e35b14a071ab4004bc12e31ddc69b869c28a1a58
CristianCaro915 <c.caro@uniandes.edu.co>
2023-09-29 18:20:19 -0500

    rrr

--- cita/models.py
+++ cita/models.py
@@ -3,10 +3,11 @@ from medico.models import Medico
 from horario.models import Horario
 
 class Cita(models.Model):
+    id=models.IntegerField(primary_key=True)
     medico = models.ForeignKey(Medico, on_delete=models.CASCADE,default=None)
     horario =models.ForeignKey(Horario, on_delete=models.CASCADE, default=None)
     date = models.DateField()
     nombre = models.CharField(max_length=50)
     apellido = models.CharField(max_length=50)
     def __str__(self):
-        return {'medico':self.medico,'horario':self.horario,'date':self.date,'nombre':self.nombre,'apellido':self.apellido}
+        return {'id':self.id,'medico':self.medico,'horario':self.horario,'date':self.date,'nombre':self.nombre,'apellido':self.apellido}
--- horario/models.py
+++ horario/models.py
@@ -2,8 +2,9 @@ from django.db import models
 from medico.models import Medico
 
 class Horario(models.Model):
-    medico = models.DecimalField(Medico.id)
+    id=models.IntegerField(primary_key=True)
+    medico = models.IntegerField(10)
     date = models.DateField()
 
     def __str__(self):
-        return {'medico':self.medico,'date':self.date}
+        return {'id':self.id,'medico':self.medico,'date':self.date}
