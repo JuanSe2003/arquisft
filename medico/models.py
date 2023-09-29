e4751ccf474a30df23a915f1d29912d195cc4b55
CristianCaro915 <c.caro@uniandes.edu.co>
2023-09-29 18:58:30 -0500

    ttt

--- medico/models.py
+++ medico/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 class Medico(models.Model):
-    id=models.IntegerField()
+    numero=models.IntegerField()
     name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     
     def __str__(self):
-        return {'id':self.id,'name':self.name,'last_name':self.last_name}
+        return {'id':self.numero,'name':self.name,'last_name':self.last_name}
 
     
