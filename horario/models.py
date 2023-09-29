485951bc2dfdfdd72e6248632b1700dc39ff324c
CristianCaro915 <c.caro@uniandes.edu.co>
2023-09-29 18:35:44 -0500

    eeee

--- horario/models.py
+++ horario/models.py
@@ -3,8 +3,8 @@ from medico.models import Medico
 
 class Horario(models.Model):
     id=models.IntegerField(primary_key=True)
-    profesional = models.IntegerField()
+    profesional = models.ForeignKey(Medico, on_delete=models.CASCADE, default=None)
     date = models.DateField()
 
     def __str__(self):
-        return {'id':self.id,'medico':self.profesional,'date':self.date}
+        return {'id':self.id,'profesional':self.profesional,'date':self.date}
