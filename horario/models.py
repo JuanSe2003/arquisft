bce6534243af777f97b277eac0184023908f28a0
CristianCaro915 <c.caro@uniandes.edu.co>
2023-09-29 18:22:56 -0500

    ttt

--- horario/models.py
+++ horario/models.py
@@ -3,7 +3,7 @@ from medico.models import Medico
 
 class Horario(models.Model):
     id=models.IntegerField(primary_key=True)
-    medico = models.IntegerField(10)
+    medico = models.IntegerField()
     date = models.DateField()
 
     def __str__(self):
