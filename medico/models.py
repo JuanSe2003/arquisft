f5c91235c11cd6b29750459d90f18d2997251b4e
CristianCaro915 <c.caro@uniandes.edu.co>
2023-09-29 18:56:47 -0500

    uuu

--- medico/models.py
+++ medico/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Medico(models.Model):
-    id=models.IntegerField(primary_key=True)
+    id=models.IntegerField()
     name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     
