a8b3d07944c8731fd4139a3a2b3791367723402e
CristianCaro915 <c.caro@uniandes.edu.co>
2023-09-30 00:14:52 -0500

    rrr

--- app/urls.py
+++ app/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('', include('medico.urls')),
     path('', include('cita.urls')),
     path('', include('horario.urls')),
+    path('health-check/', views.healthCheck),
 ]
