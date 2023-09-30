4b146db433baaa70245eb6ecfbe0328d998efe51
CristianCaro915 <c.caro@uniandes.edu.co>
2023-09-29 22:33:52 -0500

    aaa

--- horario/models.py
+++ horario/models.py
@@ -4,7 +4,7 @@ from medico.models import Medico
 class Horario(models.Model):
     id=models.IntegerField(primary_key=True)
     profesional = models.ForeignKey(Medico, on_delete=models.CASCADE,default=None)
-    hora = models.IntegerField()
+    hora = models.IntegerField(default=0)
 
     def __str__(self):
         return f'ID: {self.id}, hora:{self.hora}'
