8aecc6c030445b8cd1845c7e52a2349c312894df
Juan Sebastian Pedraza Perez <js.pedrazap1@uniandes.edu.co>
2023-09-29 21:01:07 -0500

    Test enesimo

--- horario/models.py
+++ horario/models.py
@@ -4,7 +4,7 @@ from medico.models import Medico
 class Horario(models.Model):
     id=models.IntegerField(primary_key=True)
     profesional = models.ForeignKey(Medico, on_delete=models.CASCADE,default=None)
-    time = models.CharField()
+    time = models.CharField(max_length=4)
 
     def __str__(self):
         return f'ID: {self.id}, Time: {self.time}'
