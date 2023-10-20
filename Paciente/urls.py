03b1070b35b233feada0f01a7fae6fd7eebd5c85
CristianCaro915 <c.caro@uniandes.edu.co>
2023-10-19 20:20:00 -0500

    jjj

--- Paciente/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from django.views.decorators.csrf import csrf_exempt
-from . import views
-
-urlpatterns = [
-    path('pacientes/', views.paciente_list, name='pacienteList'),
-    path('pacientecreate/', csrf_exempt(views.paciente_create), name='pacienteCreate'),
-]
