b49af5745c95b8d0e7bda3cc51a1062c794d5b93
CristianCaro915 <c.caro@uniandes.edu.co>
2023-10-19 21:54:27 -0500

    ñññ

--- horario/models.py
+++ horario/models.py
@@ -4,7 +4,7 @@ from medico.models import Medico
 class Horario(models.Model):
     id=models.IntegerField(primary_key=True)
     profesional = models.ForeignKey(Medico, on_delete=models.CASCADE,default=None)
-    date = models.DateField(default='MM-DD-YYYY')
+    date = models.DateField(default='2000-01-01')
     hora_inicio = models.IntegerField(default=0)
     hora_fin = models.IntegerField(default=0)
     disponible = models.BooleanField(default=True)
