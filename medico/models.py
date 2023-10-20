c252a806bc98b790e63b927cf20d2f480b5df332
CristianCaro915 <c.caro@uniandes.edu.co>
2023-10-19 21:39:34 -0500

    tt

--- medico/models.py
+++ medico/models.py
@@ -5,7 +5,7 @@ class Medico(models.Model):
     name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     especialidad = models.CharField(max_length=200)
-    consultorio = models.CharField(max_length=50)
+    consultorio = models.CharField(max_length=50,default="A00")
     
     def __str__(self):
         return f'{self.name} {self.last_name} {self.especialidad}'
