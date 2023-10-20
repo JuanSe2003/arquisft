bf423c87a2201304f5590d18d66d854c79773313
CristianCaro915 <c.caro@uniandes.edu.co>
2023-10-19 21:37:27 -0500

    tre

--- horario/models.py
+++ horario/models.py
@@ -7,7 +7,7 @@ class Horario(models.Model):
     date = models.DateField(default='2000-01-01')
     hora_inicio = models.IntegerField(default=0)
     hora_fin = models.IntegerField(default=0)
-    disponible = models.BooleanField()
+    disponible = models.BooleanField(default=True)
 
     def __str__(self):
         return f'{self.date} {self.hora_inicio} {self.hora_fin}'
