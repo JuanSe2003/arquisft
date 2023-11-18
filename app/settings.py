7fa0a19d44efee77173b62d6e39f403c13e0a7e3
Juan Sebastian Pedraza Perez <js.pedrazap1@uniandes.edu.co>
2023-11-17 22:19:45 -0500

    culquier cosa

--- app/settings.py
+++ app/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'medico',
     'horario',
     'cita',
-
+    'social_django',
 ]
 
 MIDDLEWARE = [
@@ -81,10 +81,10 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'monitoring_db3',
+        'NAME': 'monitoring_db',
         'USER': 'monitoring_user',
         'PASSWORD': 'isis2503',
-        'HOST': '10.128.0.6',
+        'HOST': '10.128.0.2',
         'PORT': '5432',
     }
 }
@@ -136,4 +136,13 @@ MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'static', 'media')
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
-
+LOGIN_URL = "/login/auth0"
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "https://rasiedicalinnovatech.us.auth0.com/v2/logout?returnTo=http%3A%2F%2F35.188.38.118:8080"
+SOCIAL_AUTH_TRAILING_SLASH = False
+ # Remove end slash from routes
+SOCIAL_AUTH_AUTH0_DOMAIN = 'rasiedicalinnovatech.us.auth0.com'
+SOCIAL_AUTH_AUTH0_KEY = '2PwJAJTA7YVwv7M6xcjl0l64UkhvWidk'
+SOCIAL_AUTH_AUTH0_SECRET = '5WD2glAMcET1KnzRKknUp6cg3wAM5PNHQl1ljNAgUlLC3TM5MEWwrcmQ_koFdOp9'
+SOCIAL_AUTH_AUTH0_SCOPE = [ 'openid', 'profile','email','role', ]
+AUTHENTICATION_BACKENDS = { 'app.auth0backend.Auth0', 'django.contrib.auth.backends.ModelBackend', }
--- medico/views.py
+++ medico/views.py
@@ -1,3 +1,5 @@
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.contrib import messages
 from django.http import HttpResponseRedirect
@@ -5,31 +7,48 @@ from django.urls import reverse
 from .models import Medico
 from .tests import MedicoForm
 from .logic.medico_logic import crear_medico, get_medico, get_medicos
+from  app.auth0backend import getRole
+
+
+@login_required 
+def medico_list(request):
+        role=getRole(request)
+        if role =="supervisor":
+                medicos = get_medicos()
+                context = {'medicos':medicos}
+                return render(request, 'medicos.html', context)
+        else:
+                return HttpResponse("Usuario no autorizado")
+
+
 
 def medico_create(request):
-    if request.method == 'POST':
-        form = MedicoForm(request.POST)
-        if form.is_valid():
-            crear_medico(form)
-            messages.add_message(request, messages.SUCCESS, 'Successfully created medico')
-            return HttpResponseRedirect(reverse('medicoCreate'))
+    role=getRole(request)
+    if role =="supervisor":
+        if request.method == 'POST':
+                form = MedicoForm(request.POST)
+                if form.is_valid():
+                    crear_medico(form)
+                    messages.add_message(request, messages.SUCCESS, 'Successfully created medico')
+                    return HttpResponseRedirect(reverse('medicoCreate'))
+                else:
+                    print(form.errors)
         else:
-            print(form.errors)
+                form = MedicoForm()
+
+        context = {
+                'form': form,
+        }
+        return render(request, 'medicoCreate.html', context)
     else:
-        form = MedicoForm()
+        return HttpResponse("Usuario no autorizado")
 
-    context = {
-        'form': form,
-    }
-    return render(request, 'medicoCreate.html', context)
-
-def medico_list(request):
-    medicos = get_medicos()
-    context = {'medicos':medicos}
-    return render(request, 'medicos.html', context)
-
-#¿?
 def get_medico(request):
     medico = get_medico()
     context = {'medico':medico}
     return render(request, 'medico_list.html', context)
+
+#def medico_list(request):
+   #medicos = get_medicos()
+   #context = {'medicos':medicos}
+   #return render(request, 'medicos.html', context)
