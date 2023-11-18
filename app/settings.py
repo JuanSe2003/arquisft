3479e828d80766c88a4cefd539d02c2d7f357119
Julian Escobar <escobarjulian347@gmail.com>
2023-11-18 10:59:01 -0500

    Cambio ip

--- app/settings.py
+++ app/settings.py
@@ -138,7 +138,7 @@ STATICFILES_DIRS = (
 )
 LOGIN_URL = "/login/auth0"
 LOGIN_REDIRECT_URL = "/"
-LOGOUT_REDIRECT_URL = "https://rasiedicalinnovatech.us.auth0.com/v2/logout?returnTo=http%3A%2F%2F35.188.38.118:8080"
+LOGOUT_REDIRECT_URL = "https://rasiedicalinnovatech.us.auth0.com/v2/logout?returnTo=http%3A%2F%2F34.16.8.20:8080"
 SOCIAL_AUTH_TRAILING_SLASH = False
  # Remove end slash from routes
 SOCIAL_AUTH_AUTH0_DOMAIN = 'rasiedicalinnovatech.us.auth0.com'
